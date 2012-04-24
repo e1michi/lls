47d8378aa1c46a33bb1f28560c8b0b884bc78d1f
Kaz Takahashi <kaz@mac-mini.local>
2012-04-25 03:03:40 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LLS::Application.config.secret_token = 'dae39833db1bf1d20d84b95e885e4ee2f66dea3b7b1c4fcea2cfa0664b1db6b4d0df47247a63b08f3994a5306d20201a0a6f31e6c362ba994afd16dc1755d459'
